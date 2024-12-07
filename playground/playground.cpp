26022ad600a66b1bbde1e98d517bb9793f3a8d4d
hoho965191737 <965191737@qq.com>
2024-12-07 21:16:39 +0800

    draw a matrix

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -7,6 +7,7 @@
 GLFWwindow* window;
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
 #include <common/shader.hpp>
@@ -57,8 +58,21 @@ int main( void )
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
-	GLuint programID = LoadShaders("../tutorial02_red_triangle/SimpleVertexShader.vertexshader", "../tutorial02_red_triangle/SimpleFragmentShader.fragmentshader");
+	GLuint programID = LoadShaders("../tutorial03_matrices/SimpleTransform.vertexshader", "../tutorial03_matrices/SingleColor.fragmentshader");
 
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	glm::mat4 View = glm::lookAt(
+		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+		glm::vec3(0, 0, 0), // and looks at the origin
+		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	glm::mat4 Model = glm::mat4(1.0f);
+
+	glm::mat4 MVP = Projection * View * Model;
+	
+	
 	static const GLfloat g_vertex_buffer_data[] = {
 		-1.0f, -1.0f, 0.0f,
 		 1.0f, -1.0f, 0.0f,
@@ -75,6 +89,8 @@ int main( void )
 
 		// Draw Start ------------------------------
 		glUseProgram(programID);
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -100,6 +116,11 @@ int main( void )
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
